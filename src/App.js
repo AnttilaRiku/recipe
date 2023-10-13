3a8ada8db6a49d2d809f7e7b849447d1b2a42c51
AnttilaRiku <127877164+AnttilaRiku@users.noreply.github.com>
2023-10-13 14:37:07 +0300

    Functional version without search feat

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { useState, useEffect } from 'react';
 
-const randomURL = 'https://api.spoonacular.com/recipes/random?apiKey=f10bc06bc4814ba7bde4690e75679ea1';
-const dessertURL = 'https://api.spoonacular.com/recipes/random?apiKey=f10bc06bc4814ba7bde4690e75679ea1&tags=dessert'
+//backup apiKey = 836b05dc277b4b069872ca76bb337dbc
+//og apiKey = f10bc06bc4814ba7bde4690e75679ea1
 
+const apiKey = '836b05dc277b4b069872ca76bb337dbc';
+const randomURL = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`;
+const dessertURL = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&tags=dessert`;
 
 function App() {
   const [recipe, setRecipe] = useState(null);
 
+  //Random Generated food
   async function getRandomRecipe() {
     try {
       const response = await axios.get(randomURL);
@@ -19,7 +23,7 @@ function App() {
       console.error(error);
     }
   }
-
+  //Dessert
   async function getDessertRecipe() {
     try {
       const response = await axios.get(dessertURL);
@@ -30,45 +34,52 @@ function App() {
     }
   }
 
+ 
   useEffect(() => {
     getRandomRecipe();
   }, []);
 
-
   return (
-    <div className='container'>
-      <h1>Meal of the day</h1>
-      <div className='btn-container'>
-        <br></br>
-        <button class="btn btn-secondary" onClick={getRandomRecipe}>Generate your random meal of the day!</button>
-        <button className="btn btn-secondary" style={{ marginLeft: '10px' }} onClick={getDessertRecipe}>
-          Get a dessert recipe
-        </button>
-      </div>
-      {recipe && (
-        <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image} alt={recipe.title} style={{ maxWidth: '100%', height: 'auto', marginTop: '20px', marginBottom: '20px' }} />
-          <div className="ingredients">
-            <div>
-              <h3>Ingredients needed:</h3>
-            </div>
-            <ul>
-              {recipe.extendedIngredients?.map((ingredient, index) => (
-                <li key={index}>{ingredient.original}</li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h3>Instrunctions:</h3>
-          </div>
-          <ol>
-            {recipe?.analyzedInstructions[0]?.steps?.map((step, index) => (
-              <li key={index}>{step.step}</li>
-            ))}
-          </ol>
+    <div>
+      {/* Navbar */}
+      <nav class="navbar navbar-light" style={{ backgroundColor: '#f8f3f4' }}>
+        <h1>Meal of the day</h1>    
+          {/*Random buttons and Dessert recipe */}
+        <div className='btn-container'>
+          <button class="btn btn-secondary" onClick={getRandomRecipe}>Generate your random meal of the day!</button>
+          <button className="btn btn-secondary" style={{ marginLeft: '10px' }} onClick={getDessertRecipe}>
+            Get a dessert recipe
+          </button>
         </div>
-      )}
+      </nav>
+      {/* Recipe here with bootstrap and incredients */}
+      <div className='container'>
+        {recipe && (
+          <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} style={{ maxWidth: '100%', height: 'auto', marginTop: '20px', marginBottom: '20px' }} />
+            <div className="ingredients">
+              <div>
+                <h3>Ingredients needed:</h3>
+              </div>
+              <ul>
+                {recipe.extendedIngredients?.map((ingredient, index) => (
+                  <li key={index}>{ingredient.original}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              {/* Instructions step by step numeric listing */}
+              <h3>Instructions:</h3>
+            </div>
+            <ol>
+              {recipe?.analyzedInstructions[0]?.steps?.map((step, index) => (
+                <li key={index}>{step.step}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
